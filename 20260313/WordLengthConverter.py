fe0ba0a819dc7c12d9b19c135c2350c1d57b7e7f
Vincent Xiong <xiongzk86@gmail.com>
2026-03-23 14:36:58 +1100

    Enhance convert_words function with detailed comments
    Added comments explaining the logic behind the convert_words function.

--- 20260313/WordLengthConverter.py
+++ 20260313/WordLengthConverter.py
@@ -17,3 +17,9 @@ def convert_words(s):
 # A more pythonic way to the challenge:
 def convert_words(s):
     return " ".join(str(len(word)) for word in s.split())
+
+# Although the code is only 1 line, it contains several layers of logics.
+# 1. The input string s is split into words according to space (default method in .split(), so there is no need to write s.split(" "))
+# 2. Each word is looped and its length is counted.
+# 3. The length of each word is converted into string rather than integer.
+# 4. All the lengths are joined together with spaces. Previously I used (+=) which as ChatGPT suggested is not efficient.
